6e89ffb550c16ba6a48d90dec01bec924caf8711
ShotaTokuyama <tokush1031@gmail.com>
2023-04-09 19:43:22 +0900

    Express

---
+++ routes/user.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const router = express.Router();
+
+// router.use(mylogger);
+
+router.get("/", mylogger, (req, res) => {
+    res.send("ユーザーです")
+});
+
+router.get("/info", (req, res) => {
+    res.send("ユーザー情報です")
+});
+
+router.post("/info", (req, res) => {
+    res.send("ユーザー情報です")
+});
+
+router.delete("/:id", (req, res) => {
+    res.send(`${req.params.id}のユーザー情報を取得しました。`)
+});
+
+// ミドルウェア
+function mylogger(req, res, next) {
+    console.log(req.originalUrl);
+    next();
+};
+
+module.exports = router;
